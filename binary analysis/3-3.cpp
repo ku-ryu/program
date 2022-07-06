44638af067b8174de7b969e6082c2d49158de232
ku-ryu <kubo.ryu.826@gmail.com>
2022-07-06 18:22:18 +0900

    git commit

---
+++ binary analysis/3-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    std::string input;
+    std::cin >> input;
+
+    if(input == "\x01\x02\x03\x04\x05"){
+        std::cout << "success" << std::endl;
+    } else {
+        std::cout << "error" << std::endl;
+    }
+    return 0;
+}
---
+++ binary analysis/3-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(){
+    int32_t *c = malloc(sizeof(int32_t));
+    *c = 0x65746166;
+
+    printf("%s\n", (char *)c);
+
+}
