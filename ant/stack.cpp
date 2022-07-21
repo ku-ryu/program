f78b2972cfb51cb42223216cd1e16aed16457ba8
ku-ryu <kubo.ryu.826@gmail.com>
2022-07-21 15:08:48 +0900

    git commit

---
+++ ant/stack.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    return 0;
+}
---
+++ ant/再帰関数.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int memo[11];
+int fib(int n){
+    if(n <= 1) return n;
+    if(memo[n] != 0) return memo[n];
+    return memo[n] = fib(n-1) + fib(n-2);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+}
